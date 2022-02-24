11ebfe9fc49c11ee17b0d6ab91359ee9f644662b
amandairizaga <97555273+amandairizaga@users.noreply.github.com>
2022-02-24 12:01:10 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+
+function preload(){
+pathImg = loadImage("path.png");
+boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+ createCanvas(400,400);
+
+// Movendo o fundo 
+
+
+
+
+//Criando menino que corre 
+
+
+
+// Criando Boundary (Limite) esquerdo  
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible = false;
+
+
+//Crie Boundary direito 
+
+}
+
+
+function draw() {
+background(0);
+path.velocityY = 4;
+
+// Menino se movendo no eixo X com o mouse
+edges= createEdgeSprites();
+boy.collide(edges[3]);
+
+
+
+ //Reiniciar o fundo
+ 
+
+
+
+ drawSprites();
+
+ }
+
+
+
+
